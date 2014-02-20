628e9e617bb9a211f454c17e4d3d3075cdd6891b
Vincent Rioux <riov01@siq.gouv.qc.ca>
2014-02-20 15:52:43 -0500

    Ajouter test

---
+++ ATMTECH.Achievement.Tests/DAO/DAOAccomplissementTest.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using ATMTECH.Achievement.DAO;
+using ATMTECH.Achievement.Entities;
+using ATMTECH.Shell.Tests;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Ploeh.AutoFixture;
+
+namespace ATMTECH.Achievement.Tests.DAO
+{
+    [TestClass]
+    public class DAOAccomplissementTest : BaseDaoTest<DAOAccomplissement>
+    {
+        [TestInitialize()]
+        public void Initialize()
+        {
+            CreerDatabaseTest("ATMTECH.Achievement.Entities");
+        }
+
+   
+    }
+}
+
