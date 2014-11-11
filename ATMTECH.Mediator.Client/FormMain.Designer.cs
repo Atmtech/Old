c3a4c34b054f13857780fe0c396a8bbdeeb59972
Vincent Rioux <riov01@siq.gouv.qc.ca>
2014-11-11 11:43:53 -0500

    Ajouter grossir caractère

--- ATMTECH.Mediator.Client/FormMain.Designer.cs
+++ ATMTECH.Mediator.Client/FormMain.Designer.cs
@@ -34,6 +34,9 @@
             this.textBoxClavardage = new System.Windows.Forms.TextBox();
             this.timerClavardage = new System.Windows.Forms.Timer(this.components);
             this.btnDernierClavardage = new System.Windows.Forms.Button();
+            this.timerFocus = new System.Windows.Forms.Timer(this.components);
+            this.btnZoomPlus = new System.Windows.Forms.Button();
+            this.btnZoomMoins = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // richTextBoxClavardage
@@ -45,11 +48,11 @@
             this.richTextBoxClavardage.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.richTextBoxClavardage.Cursor = System.Windows.Forms.Cursors.IBeam;
             this.richTextBoxClavardage.ForeColor = System.Drawing.Color.White;
-            this.richTextBoxClavardage.Location = new System.Drawing.Point(0, 12);
+            this.richTextBoxClavardage.Location = new System.Drawing.Point(3, 7);
             this.richTextBoxClavardage.Name = "richTextBoxClavardage";
             this.richTextBoxClavardage.ReadOnly = true;
             this.richTextBoxClavardage.ShowSelectionMargin = true;
-            this.richTextBoxClavardage.Size = new System.Drawing.Size(840, 175);
+            this.richTextBoxClavardage.Size = new System.Drawing.Size(836, 188);
             this.richTextBoxClavardage.TabIndex = 0;
             this.richTextBoxClavardage.Text = "";
             // 
@@ -60,10 +63,10 @@
             this.textBoxClavardage.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
             this.textBoxClavardage.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.textBoxClavardage.ForeColor = System.Drawing.Color.White;
-            this.textBoxClavardage.Location = new System.Drawing.Point(4, 195);
+            this.textBoxClavardage.Location = new System.Drawing.Point(5, 196);
             this.textBoxClavardage.Margin = new System.Windows.Forms.Padding(5);
             this.textBoxClavardage.Name = "textBoxClavardage";
-            this.textBoxClavardage.Size = new System.Drawing.Size(757, 20);
+            this.textBoxClavardage.Size = new System.Drawing.Size(662, 20);
             this.textBoxClavardage.TabIndex = 1;
             this.textBoxClavardage.KeyUp += new System.Windows.Forms.KeyEventHandler(this.textBoxChat_KeyUp);
             // 
@@ -75,16 +78,52 @@
             // btnDernierClavardage
             // 
             this.btnDernierClavardage.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnDernierClavardage.FlatAppearance.BorderSize = 0;
+            this.btnDernierClavardage.FlatAppearance.BorderColor = System.Drawing.Color.Gray;
             this.btnDernierClavardage.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDernierClavardage.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnDernierClavardage.ForeColor = System.Drawing.Color.White;
-            this.btnDernierClavardage.Location = new System.Drawing.Point(764, 194);
+            this.btnDernierClavardage.Location = new System.Drawing.Point(745, 195);
             this.btnDernierClavardage.Name = "btnDernierClavardage";
-            this.btnDernierClavardage.Size = new System.Drawing.Size(75, 22);
+            this.btnDernierClavardage.Size = new System.Drawing.Size(94, 22);
             this.btnDernierClavardage.TabIndex = 2;
             this.btnDernierClavardage.Text = "50 derniers";
             this.btnDernierClavardage.UseVisualStyleBackColor = true;
             this.btnDernierClavardage.Click += new System.EventHandler(this.btnDernierClavardage_Click);
+            // 
+            // timerFocus
+            // 
+            this.timerFocus.Enabled = true;
+            this.timerFocus.Tick += new System.EventHandler(this.timerFocus_Tick);
+            // 
+            // btnZoomPlus
+            // 
+            this.btnZoomPlus.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnZoomPlus.FlatAppearance.BorderColor = System.Drawing.Color.Gray;
+            this.btnZoomPlus.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnZoomPlus.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnZoomPlus.ForeColor = System.Drawing.Color.White;
+            this.btnZoomPlus.Location = new System.Drawing.Point(670, 195);
+            this.btnZoomPlus.Name = "btnZoomPlus";
+            this.btnZoomPlus.Size = new System.Drawing.Size(35, 22);
+            this.btnZoomPlus.TabIndex = 3;
+            this.btnZoomPlus.Text = "A+";
+            this.btnZoomPlus.UseVisualStyleBackColor = true;
+            this.btnZoomPlus.Click += new System.EventHandler(this.btnZoomPlus_Click);
+            // 
+            // btnZoomMoins
+            // 
+            this.btnZoomMoins.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnZoomMoins.FlatAppearance.BorderColor = System.Drawing.Color.Gray;
+            this.btnZoomMoins.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnZoomMoins.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnZoomMoins.ForeColor = System.Drawing.Color.White;
+            this.btnZoomMoins.Location = new System.Drawing.Point(706, 195);
+            this.btnZoomMoins.Name = "btnZoomMoins";
+            this.btnZoomMoins.Size = new System.Drawing.Size(38, 22);
+            this.btnZoomMoins.TabIndex = 4;
+            this.btnZoomMoins.Text = "A-";
+            this.btnZoomMoins.UseVisualStyleBackColor = true;
+            this.btnZoomMoins.Click += new System.EventHandler(this.btnZoomMoins_Click);
             // 
             // FormMain
             // 
@@ -92,11 +131,14 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
             this.ClientSize = new System.Drawing.Size(841, 219);
+            this.Controls.Add(this.btnZoomMoins);
+            this.Controls.Add(this.btnZoomPlus);
             this.Controls.Add(this.btnDernierClavardage);
             this.Controls.Add(this.textBoxClavardage);
             this.Controls.Add(this.richTextBoxClavardage);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "FormMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Mediator";
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.FormMain_FormClosed);
             this.Load += new System.EventHandler(this.FormMain_Load);
@@ -111,6 +153,9 @@
         private System.Windows.Forms.TextBox textBoxClavardage;
         private System.Windows.Forms.Timer timerClavardage;
         private System.Windows.Forms.Button btnDernierClavardage;
+        private System.Windows.Forms.Timer timerFocus;
+        private System.Windows.Forms.Button btnZoomPlus;
+        private System.Windows.Forms.Button btnZoomMoins;
     }
 }
 
--- ATMTECH.Mediator.Client/FormMain.cs
+++ ATMTECH.Mediator.Client/FormMain.cs
@@ -6,7 +6,7 @@ namespace ATMTECH.Mediator.Client
     public partial class FormMain : Form
     {
         private GestionPresentation _gestionPresentation;
-        public GestionPresentation GestionPresentation { get { return _gestionPresentation ?? (_gestionPresentation = new GestionPresentation()); }}
+        public GestionPresentation GestionPresentation { get { return _gestionPresentation ?? (_gestionPresentation = new GestionPresentation()); } }
 
         public FormMain()
         {
@@ -42,6 +42,23 @@ namespace ATMTECH.Mediator.Client
         private void btnDernierClavardage_Click(object sender, EventArgs e)
         {
             GestionPresentation.AfficherClavardage(richTextBoxClavardage, 50);
+        }
+
+        private void timerFocus_Tick(object sender, EventArgs e)
+        {
+            textBoxClavardage.Focus();
+            timerFocus.Enabled = false;
+        }
+
+        private void btnZoomPlus_Click(object sender, EventArgs e)
+        {
+            richTextBoxClavardage.ZoomFactor += (float)0.1;
+        }
+
+        private void btnZoomMoins_Click(object sender, EventArgs e)
+        {
+            if (richTextBoxClavardage.ZoomFactor >= 0.8)
+                richTextBoxClavardage.ZoomFactor -= (float)0.1;
         }
 
 
