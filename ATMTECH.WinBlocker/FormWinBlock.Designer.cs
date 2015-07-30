4410fff05345e49045de6c7c2e92383ceb1454d5
Vincent Rioux <riov01@siq.gouv.qc.ca>
2015-07-30 10:29:16 -0400

    hum !

--- ATMTECH.WinBlocker/FormWinBlock.Designer.cs
+++ ATMTECH.WinBlocker/FormWinBlock.Designer.cs
@@ -154,8 +154,11 @@
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
             this.Name = "FormWinBlock";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "WinBlocker";
             this.Resize += new System.EventHandler(this.Form1_Resize);
