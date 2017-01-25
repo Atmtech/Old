67b82826ea442962d6970e59f506a3329bcd48c2
Vincent Rioux <riov01@siq.gouv.qc.ca>
2017-01-25 13:26:08 -0500

    batching image

---
+++ ATMTECH.ImageBatch/Form1.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace ATMTECH.ImageBatch
+{
+    partial class FormConvertir
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnConvertir = new System.Windows.Forms.Button();
+            this.txtSource = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtCible = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtDPI = new System.Windows.Forms.TextBox();
+            this.txtLargeur = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtHauteur = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtStatus = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnConvertir
+            // 
+            this.btnConvertir.BackColor = System.Drawing.Color.Gray;
+            this.btnConvertir.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnConvertir.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConvertir.ForeColor = System.Drawing.Color.White;
+            this.btnConvertir.Location = new System.Drawing.Point(436, 106);
+            this.btnConvertir.Name = "btnConvertir";
+            this.btnConvertir.Size = new System.Drawing.Size(112, 32);
+            this.btnConvertir.TabIndex = 0;
+            this.btnConvertir.Text = "CONVERTIR";
+            this.btnConvertir.UseVisualStyleBackColor = false;
+            this.btnConvertir.Click += new System.EventHandler(this.btnConvertir_Click);
+            // 
+            // txtSource
+            // 
+            this.txtSource.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSource.Location = new System.Drawing.Point(115, 12);
+            this.txtSource.Name = "txtSource";
+            this.txtSource.Size = new System.Drawing.Size(433, 22);
+            this.txtSource.TabIndex = 1;
+            this.txtSource.Text = "C:\\Users\\riov01\\iCloudDrive\\Dans le temps de l\'écrire\\Originale";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(15, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(101, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Répertoire source:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(15, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(91, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Répertoire cible:";
+            // 
+            // txtCible
+            // 
+            this.txtCible.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCible.Location = new System.Drawing.Point(115, 40);
+            this.txtCible.Name = "txtCible";
+            this.txtCible.Size = new System.Drawing.Size(433, 22);
+            this.txtCible.TabIndex = 4;
+            this.txtCible.Text = "C:\\Users\\riov01\\iCloudDrive\\Dans le temps de l\'écrire\\Ajuste";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(15, 73);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(27, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "DPI:";
+            // 
+            // txtDPI
+            // 
+            this.txtDPI.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDPI.Location = new System.Drawing.Point(115, 68);
+            this.txtDPI.Name = "txtDPI";
+            this.txtDPI.Size = new System.Drawing.Size(88, 22);
+            this.txtDPI.TabIndex = 6;
+            this.txtDPI.Text = "400";
+            // 
+            // txtLargeur
+            // 
+            this.txtLargeur.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtLargeur.Location = new System.Drawing.Point(278, 68);
+            this.txtLargeur.Name = "txtLargeur";
+            this.txtLargeur.Size = new System.Drawing.Size(88, 22);
+            this.txtLargeur.TabIndex = 8;
+            this.txtLargeur.Text = "5000";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(209, 73);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(49, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Largeur:";
+            // 
+            // txtHauteur
+            // 
+            this.txtHauteur.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtHauteur.Location = new System.Drawing.Point(460, 68);
+            this.txtHauteur.Name = "txtHauteur";
+            this.txtHauteur.Size = new System.Drawing.Size(88, 22);
+            this.txtHauteur.TabIndex = 10;
+            this.txtHauteur.Text = "5000";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(388, 73);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(52, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Hauteur:";
+            // 
+            // txtStatus
+            // 
+            this.txtStatus.Location = new System.Drawing.Point(18, 152);
+            this.txtStatus.Multiline = true;
+            this.txtStatus.Name = "txtStatus";
+            this.txtStatus.Size = new System.Drawing.Size(530, 184);
+            this.txtStatus.TabIndex = 11;
+            // 
+            // FormConvertir
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(25)))), ((int)(((byte)(25)))));
+            this.ClientSize = new System.Drawing.Size(566, 348);
+            this.Controls.Add(this.txtStatus);
+            this.Controls.Add(this.txtHauteur);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtLargeur);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtDPI);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtCible);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtSource);
+            this.Controls.Add(this.btnConvertir);
+            this.Name = "FormConvertir";
+            this.Text = "Image batch processing";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnConvertir;
+        private System.Windows.Forms.TextBox txtSource;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtCible;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtDPI;
+        private System.Windows.Forms.TextBox txtLargeur;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtHauteur;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtStatus;
+    }
+}
+
---
+++ ATMTECH.ImageBatch/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ATMTECH.ImageBatch
+{
+    public partial class FormConvertir : Form
+    {
+        public FormConvertir()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConvertir_Click(object sender, EventArgs e)
+        {
+            Convertir();
+        }
+
+        public void Convertir()
+        {
+            string repertoireSource = txtSource.Text;
+            string repertoireCible = txtCible.Text;
+
+            txtStatus.Text = string.Empty;
+
+            string[] fileEntries = Directory.GetFiles(repertoireSource);
+            foreach (string fileName in fileEntries)
+            {
+                string nomFichierSource = Path.GetFileName(fileName);
+                string nomFichierCible = repertoireCible + @"\" + nomFichierSource.Replace(".jpg", ".png").Replace(".jpeg", ".png");
+                string nomFichierCibleTemp = repertoireCible + @"\Temp\" + nomFichierSource.Replace(".jpg", ".png").Replace(".jpeg", ".png");
+
+                if (File.Exists(nomFichierCible))
+                    File.Delete(nomFichierCible);
+
+                txtStatus.Text += nomFichierSource + Environment.NewLine;
+
+                using (Bitmap bitmap = (Bitmap)Image.FromFile(fileName))
+                {
+                    using (Bitmap newBitmap = new Bitmap(bitmap))
+                    {
+                        newBitmap.SetResolution(Convert.ToSingle(txtDPI.Text), Convert.ToSingle(txtDPI.Text));
+                        newBitmap.Save(nomFichierCibleTemp, ImageFormat.Png);
+                        using (var image = Image.FromFile(nomFichierCibleTemp))
+                        using (var newImage = ScaleImage(image, Convert.ToInt32(txtLargeur.Text), Convert.ToInt32(txtHauteur.Text)))
+                        {
+                            newImage.Save(nomFichierCible, ImageFormat.Png);
+                        }
+                    }
+                }
+            }
+        }
+
+        public static Image ScaleImage(Image image, int maxWidth, int maxHeight)
+        {
+            var ratioX = (double)maxWidth / image.Width;
+            var ratioY = (double)maxHeight / image.Height;
+            var ratio = Math.Min(ratioX, ratioY);
+
+            var newWidth = (int)(image.Width * ratio);
+            var newHeight = (int)(image.Height * ratio);
+
+            var newImage = new Bitmap(newWidth, newHeight);
+
+            using (var graphics = Graphics.FromImage(newImage))
+                graphics.DrawImage(image, 0, 0, newWidth, newHeight);
+
+            return newImage;
+        }
+
+    }
+}
